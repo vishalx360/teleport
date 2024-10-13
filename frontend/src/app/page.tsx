f5bc8ad5a7c4c748a5e68ce61179344635414964
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2024-10-13 15:38:22 +0530

    Add UI components and dependencies Add homepage temp ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,151 @@
-import Image from "next/image";
+'use client'
+import Link from "next/link";
+
+import { Address } from "@/components/Address";
+import BackButton from "@/components/BackButton";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowRight, InfoIcon, Package, UserCircle } from "lucide-react";
+import LinkButton from "@/components/LinkButton";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [pickup, setPickUp] = useState<Address | null>(null);
+  const [delivery, setDelivery] = useState<Address | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <Card className="max-w-md mx-auto">
+        <CardHeader className='gap-2'>
+          <div className="flex items-center justify-between">
+            <CardTitle className="text-2xl font-bold mt-2">
+              <Package className="inline" /> Teleport
+            </CardTitle>
+            <Button onClick={null} variant="ghost" className="flex items-center gap-4">
+              <span className="font-bold">
+                {"Username"}
+              </span>
+              <UserCircle />
+              <span className="sr-only">Go back</span>
+            </Button>
+          </div>
+          <CardDescription className="text-sm text-gray-600">
+            Send anything, anywhere
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+
+          <NewBookingSection />
+          <PastbookingsSection />
+
+        </CardContent>
+        <CardFooter>
+
+        </CardFooter>
+      </Card>
     </div>
   );
 }
+
+function NewBookingSection() {
+  return (
+    <div >
+      <LinkButton href="/booking" variant={"outline"}
+        className="rounded-xl p-4 px-5 flex items-center justify-between h-auto w-full bg-blue-500 text-white hover:bg-blue-600 transition-colors hover:text-white">
+        <div>
+          <h3 className="font-bold text-xl">
+            Pick up or Send anything
+          </h3>
+          <p className="text-gray-100">
+            Enter pick up and delivery locations
+          </p>
+        </div>
+        <ArrowRight />
+      </LinkButton>
+    </div>)
+}
+
+type Booking = {
+  id: number;
+  from: string;
+  to: string;
+  cost: number;
+  date: Date;
+}
+
+function PastbookingsSection() {
+  const [pastBookings, setPastBookings] = useState<Array<Booking>>([
+    {
+      id: 1,
+      from: "Home",
+      to: "Work",
+      cost: 200,
+      date: new Date()
+    },
+    {
+      id: 2,
+      from: "Home",
+      to: "Work",
+      cost: 200,
+      date: new Date()
+    },
+    {
+      id: 3,
+      from: "Home",
+      to: "Work",
+      cost: 200,
+      date: new Date()
+    },
+    {
+      id: 4,
+      from: "Home",
+      to: "Work",
+      cost: 200,
+      date: new Date()
+    },
+    {
+      id: 5,
+      from: "Home",
+      to: "Work",
+      cost: 200,
+      date: new Date()
+    },
+  ]);
+  return (
+    <div>
+      <h3 className="font-bold text">
+        Past Bookings
+      </h3>
+      {pastBookings.length === 0 ? (
+        <div className="text-center text-sm text-gray-600 bg-gray-200 p-5 rounded-xl">No past bookings</div>
+      ) : (
+        <div className="overflow-y-scroll h-[50vh] flex mt-2 flex-col gap-4">
+          {pastBookings.map((booking) => (
+            <PastBookingCard key={booking.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+function PastBookingCard() {
+  return (
+    <Link href="/booking/123" className="rounded-xl p-4 px-5 flex items-center justify-between h-auto w-full  transition-colors bg-gray-100 hover:bg-gray-200">
+      <div>
+        <p className="font-bold text-sm ">
+          <Package className="inline h-4 w-4" /> Booking ID: #34234
+        </p>
+        <h3 className="text-sm">
+          5 days ago
+        </h3>
+        <p className="text-xs text-gray-600">
+          From: Home, To: Work Rs. 200
+        </p>
+
+      </div>
+      <InfoIcon />
+    </Link>
+  )
+}
+
